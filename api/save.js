cbf28d16cc086a1deb762fba19cec242188e2ba7
amiralisedamir1389 <bagherisedamir@gmail.com>
2026-07-18 05:07:14 +0330

    Update save.js

--- api/save.js
+++ api/save.js
@@ -3,6 +3,15 @@ import { MongoClient } from 'mongodb';
 const uri = "mongodb+srv://sedamirbagheri_db_user:sedamir1389@cluster0.jdrrw4z.mongodb.net/?appName=Cluster0";
 
 export default async function handler(req, res) {
+    // تنظیمات CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
